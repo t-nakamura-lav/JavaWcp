1ba98e9b142ab3bc32d12b004f5b33facc8e6ec3
Tamae Nakamura <1am1goodtime@gmail.com>
2021-09-10 17:57:27 +0900

    first commit

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 8;
+		String str = "ちいかわ"; // str名前を初期化
+		System.out.println(str);
+
+		str = "ありがとう！"; // str1に好きな言葉を再代入
+		System.out.println(str);
+
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
